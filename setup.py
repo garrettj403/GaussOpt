87c84b1111aa767e7bc77482d727e1acfe5c7f1d
John Garrett <john.garrett@astro.ox.ac.uk>
2019-08-10 13:28:50 -0600

    Add required packages and classifiers to setup.py [ci skip]

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-
-setup(name='GaussOpt',
-      version='1.1.3',
-      description='Gaussian beam analysis',
-      keywords='gaussian optics millimeter terahertz thz',
-      packages=['gaussopt',],
-      author='John Garrett',
-      author_email='garrettj403@gmail.com',
-      license='MIT',
-      url='https://github.com/garrettj403/GaussOpt/',
-      long_description=open('README').read(),
-     )
+setup(
+    name='GaussOpt',
+    version='1.1.3',
+    author='John Garrett',
+    author_email='garrettj403@gmail.com',
+    description='Gaussian beam analysis',
+    license='MIT',
+    keywords='gaussian optics millimeter terahertz thz',
+    url='https://github.com/garrettj403/GaussOpt/',
+    packages=['gaussopt',],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy'
+    ],
+    long_description=open('misc/README').read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+)
